0c7fad1a613d2226b896dc316a07d55d179bd0e1
Krishna gopal singh <krishnagopalsingh587@gmail.com>
2024-11-17 15:52:31 +0530

    changes

--- server.js
+++ server.js
@@ -4,33 +4,27 @@ require("dotenv").config();
 const { connectTOMongoDB } = require("./connect");
 const app = express();
 const bodyParser = require("body-parser");
-const { URL } = require("url"); // Import the URL module
+const { URL } = require("url"); 
 
 const mongoURI = process.env.MONGO_URI;
-const BASE_URL = process.env.BASE_URL; // e.g., "http://localhost:5000"
+const BASE_URL = process.env.BASE_URL;
 
-// Parse the port number from the BASE_URL
 const parsedURL = new URL(BASE_URL);
-const PORT = parsedURL.port || 80; // Default to port 80 if no port is specified
+const PORT = parsedURL.port || 80; 
 
-// Import routes
 const apiRoutes = require("./routes/route");
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
 
-// Connect to MongoDB
 connectTOMongoDB(mongoURI);
 
-// Routes
 app.get("/", (req, res) => {
   res.send("Working well");
 });
 app.use("/api", apiRoutes);
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at ${BASE_URL}`);
 });
